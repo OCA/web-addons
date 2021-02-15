aba8e5729feff0662aceedf78cac0d809c4cc310
Sergei Ruzki <sergei.ruzki@oms-pruefservice.de>
2021-02-15 14:46:56 +0100

    web_boolean_button ported from Odoo 12.0 CE

---
+++ web_boolean_button/__manifest__.py
@@ -0,0 +1,20 @@
+{
+    "name": "Boolean Button Widget",
+    "version": "13.0.0.1.0",
+    "category": "Web",
+    "summary": "",
+    "license": "LGPL-3",
+    "author": "Odoo Community Association (OCA)"
+              "Sergei Ruzki,",
+    "website": "http://bigdata.by/",
+    "depends": [
+        "web",
+    ],
+    "data": [
+        "views/templates.xml",
+    ],
+    "qweb": [],
+    "installable": True,
+    "application": False,
+    "auto_install": False,
+}
